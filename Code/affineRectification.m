b4ff9dba4463befcf82baef33e21bb51a46a4cd1
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-01-02 19:27:23 +0100

    feat: make sense of image of corners after rectification

--- Code/affineRectification.m
+++ Code/affineRectification.m
@@ -16,22 +16,36 @@ load('./CircleC.mat');
 
 %% Compute the homography for affine rectification
 l_h_inf_prime = l_h_inf_prime / l_h_inf_prime(3);
-H_aff = [0.1, 0, 0; 0, 0.1, 0; l_h_inf_prime'];
+H_aff = [.1, 0, 0; 0, .1, 0; l_h_inf_prime'];
 H_aff_inv = inv(H_aff);
 
 %% Sanity check the homography
 fprintf('Sanity Check [Affine Rectification]\nline at infinity after homography:');
-disp(warpLine(l_h_inf_prime, H_aff_inv));
+disp(warpLine(l_h_inf_prime, H_aff_inv)');
 
 %% Apply the homography to the image
 im_warped = warpImage(im, H_aff);
 
 %% Compute the image of the lines
 m_lines_aff = warpLine(m_lines, H_aff_inv);
+m_lines_aff = m_lines_aff ./ m_lines_aff(3, :);
 m_points_aff = warpPoint(m_points, H_aff);
 
 l_lines_aff = warpLine(l_lines, H_aff_inv);
+l_lines_aff = l_lines_aff ./ l_lines_aff(3, :);
 l_points_aff = warpPoint(l_points, H_aff);
+
+% Sanity check: parallel lines
+disp('Sanity Check [Affine Rectification]');
+disp('Parallel lines after affine rectification');
+m_lines_aff_angles = rad2deg(atan2(m_lines_aff(2, :), m_lines_aff(1, :)));
+l_lines_aff_angles = rad2deg(atan2(l_lines_aff(2, :), l_lines_aff(1, :)));
+
+disp('Angles of the m lines after affine rectification in degrees');
+disp(m_lines_aff_angles);
+
+disp('Angles of the l lines after affine rectification in degrees');
+disp(l_lines_aff_angles);
 
 % Compute conic Errors
 conicErrors = computeConicErrors(C, im);
@@ -73,14 +87,16 @@ C_metric_errors = computeConicErrors(C_metric, im_metric);
 
 %% Compute the images of the lines
 m_lines_metric = warpLine(m_lines, H_metric_inv);
+m_lines_metric = m_lines_metric ./ m_lines_metric(3, :);
 m_points_metric = warpPoint(m_points, H_metric);
 
 l_lines_metric = warpLine(l_lines, H_metric_inv);
+l_lines_metric = l_lines_metric ./ l_lines_metric(3, :);
 l_points_metric = warpPoint(l_points, H_metric);
 
 %% Sanity check: othogonality of rectified lines
-disp('Sanity Check: Orthogonality of rectified lines');
-disp(m_lines_metric' * l_lines_metric);
+disp('Sanity Check [Metric Rectification]: Orthogonality of rectified lines');
+disp(m_lines_metric(1:2, :)' * l_lines_metric(1:2, :));
 
 %% Show the images
 figure;
@@ -103,6 +119,12 @@ plotLines(m_points, 'g', 'm', 2);
 plotLines(l_points, 'r', 'l', 2);
 contour(conicErrors, [0, 0], 'b', 'LineWidth', 2);
 plotConicParams(center, axes, angle, 'b');
+
+% plot the line at infinity
+xs = 1:size(im, 2);
+ys = (-l_h_inf_prime(3) - l_h_inf_prime(1) * xs) / l_h_inf_prime(2);
+plot(xs, ys, 'b--', 'LineWidth', 1);
+
 title('Original Image + lines + conic');
 
 subplot(2, 3, 5);
@@ -123,6 +145,14 @@ contour(C_metric_errors, [0, 0], 'b', 'LineWidth', 2);
 plotConicParams(center_metric, axes_metric, angle_metric, 'b');
 title('Metric Rectification + lines + conic');
 
+%% Compute depth m
+m_lines_length = vecnorm(m_points_metric(:, 1:2:end) - m_points_metric(:, 2:2:end));
+l1_legnth = vecnorm(l_points_metric(:, 1) - l_points_metric(:, 2));
+average_m_length = mean(m_lines_length);
+depth_m = average_m_length / l1_legnth;
+
+disp('Depth of m');
+disp(depth_m);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                       USEFUL FUNCTIONS                              %
@@ -151,19 +181,43 @@ h = size(image, 1);
 w = size(image, 2);
 
 corners =  [
-    0, w, 0, w;
+    0, w, w, 0;
     0, 0, h, h;
-    ones(1, 4)
     ];
 
-corners_warped = H * corners;
-corners_warped = corners_warped ./ corners_warped(3, :);
+corners_warped = warpPoint(corners, H);
 
 disp('corners_warped');
 disp(corners_warped);
 
-w_warped = ceil(max(corners_warped(1, :)) - min(corners_warped(1, :)));
+% plot the corners
+figure;
+imshow(image);
+hold on;
+plot(corners(1, :), corners(2, :), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
+for i = 1:4
+    text(corners(1, i), corners(2, i), [num2str(i) ' - (' num2str(corners(1, i)) ', ' num2str(corners(2, i)) ')'], 'Color', 'r', 'FontSize', 11);
+end
+plot(corners_warped(1, :), corners_warped(2, :), 'bx', 'MarkerSize', 10, 'LineWidth', 2);
+for i = 1:4
+    text(corners_warped(1, i), corners_warped(2, i), [num2str(i) '-(' num2str(corners_warped(1, i)) ', ' num2str(corners_warped(2, i)) ')'], 'Color', 'b', 'FontSize', 11);
+end
+
+% Plot a bounding box for the warped corners
+max_x = max(corners_warped(1, :));
+min_x = min(corners_warped(1, :));
+max_y = max(corners_warped(2, :));
+min_y = min(corners_warped(2, :));
+
+line([min_x, min_x], [min_y, max_y], 'Color', 'g', 'LineWidth', 2);
+line([max_x, max_x], [min_y, max_y], 'Color', 'g', 'LineWidth', 2);
+line([min_x, max_x], [min_y, min_y], 'Color', 'g', 'LineWidth', 2);
+line([min_x, max_x], [max_y, max_y], 'Color', 'g', 'LineWidth', 2);
+hold off;
+
+
 h_warped = ceil(max(corners_warped(2, :)) - min(corners_warped(2, :)));
+w_warped = ceil(max(corners_warped(1, :)) - min(corners_warped(1, :)));
 disp('w_warped');
 disp(w_warped);
 disp('h_warped');
@@ -224,13 +278,12 @@ end
 end
 
 function plotConicParams(center, axes, angle, color)
-%% Plot the center
-plot(center(1), center(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
-
 %% Plot the axes
 roationMatrix = [cos(angle), -sin(angle); sin(angle), cos(angle)];
 axesVectors = roationMatrix * diag(axes);
 quiver(center(1), center(2), axesVectors(1, 1), axesVectors(2, 1), 'Color', color, 'LineWidth', 2);
 quiver(center(1), center(2), axesVectors(1, 2), axesVectors(2, 2), 'Color', color, 'LineWidth', 2);
 
+%% Plot the center
+plot(center(1), center(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
 end
